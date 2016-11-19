2ebad6fd41229b81191dde43a23d403b580fcd84
Krzysztof Wyrzykowski <krzysztof.wyrzykowski1989@gmail.com>
2016-11-19 13:09:05 +0100

    add dist and build

---
+++ webpack.dist.config.js
@@ -0,0 +1,54 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './main.js',
+  output: {
+    path: 'dist/assets/',
+    publicPath: 'dist/assets/',
+    filename: 'index.js'
+  },
+  devServer: {
+    inline: true,
+    port: 3333
+  },
+
+  debug: false,
+  devtool: false,
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {NODE_ENV: '"production"'}
+    }),
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new ExtractTextPlugin('styles.css'),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }, {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', '!css-loader?sourceMap')
+      }, {
+        test: /\.scss/,
+        loader: ExtractTextPlugin.extract('style-loader', '!css-loader?sourceMap!sass-loader?outputStyle=expanded')
+      }, {
+        test: /\.(otf|eot|png|svg|jpg|ttf|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]'
+      }
+    ]
+  }
+};
